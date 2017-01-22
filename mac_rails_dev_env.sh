ef06f50fe9a6f815322d2a0f9c065873fc565861
Prasad Pilla <prasad.pilla81@gmail.com>
2017-01-22 17:32:36 +0530

    Add brew install command and convert array elements to string literals

--- mac_rails_dev_env.sh
+++ mac_rails_dev_env.sh
@@ -2,15 +2,16 @@
 
 #Mac OS Rails development enviroment setup automation script for essential tools.
 
-TOOLS_ARRAY=( "XCode:xcode-select --install && brew tap homebrew/dupes && brew install libxml2 libxslt libiconv && brew update"
-            "install rvm:gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3\curl -sSL https://get.rvm.io && source ~/.rvm/scripts/rvm"      
-        "Ruby 2.3.1:rvm install 2.3.1"
-        "Rails 4.2.7:gem install rails 4.2.7"
-        "Bundler:gem install bundler "
-        "Git:brew install git"
-        "MySQL:brew install mysql && mysql.server start && mysql_secure_installation"
-        "PostgreSQL: brew install postgresql"
-        "Redis:brew install redis"
+TOOLS_ARRAY=( "XCode:xcode-select --install"
+        'install Brew:/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && brew tap homebrew/dupes && brew install libxml2 libxslt libiconv && brew update'
+        'install rvm:gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3\curl -sSL https://get.rvm.io && source ~/.rvm/scripts/rvm'      
+        'Ruby 2.3.1:rvm install 2.3.1'
+        'Rails 4.2.7:gem install rails 4.2.7'
+        'Bundler:gem install bundler'
+        'Git:brew install git'
+        'MySQL:brew install mysql && mysql.server start && mysql_secure_installation'
+        'PostgreSQL: brew install postgresql'
+        'Redis:brew install redis'
         )
 
 for tool in "${TOOLS_ARRAY[@]}" ; do
